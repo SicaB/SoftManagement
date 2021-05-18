c5dae572e05ddc43bc17f7a736bebad811fa8578
Sacha Behrend <sacha.behrend@gmail.com>
2021-05-18 09:35:32 +0200

    Initial Commit

---
+++ SoftManagement/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoftManagement
+//
+//  Created by Sacha Behrend on 18/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SoftManagement/SoftManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftManagementApp.swift
+//  SoftManagement
+//
+//  Created by Sacha Behrend on 18/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SoftManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
